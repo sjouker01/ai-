0c44bb5f99b8b01a220a53dd06ce5e43cf5ea7ff
sjouker01 <sjoukjonker13@gmail.com>
2023-03-27 23:55:20 +0200

    tr

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-const brain = require('brain.js');
+const net = new brain.NeuralNetwork()
 
-// input 1 0, output 1
-// input 1 1, output 0
+net.train([ 
+  {
+    input: [0 , 0],
+    output:[0]
+  },
+  {
+    input: [1 , 0],
+    output:[1]
+  },
+  {
+    input: [0 , 1],
+    output:[1]
+  },
+  {
+    input: [1 , 1],
+    output:[0]
+  }
+])
 
-const net = new brain.NeuralNetwork({ hiddenLayers: [3] });
+const diagram = document.getElementById('diagram')
+diagram.innerHTML = brain.utilities.toSVG(net)
 
-const trainingData = [
-  { input: [0, 0], output: [0] },
-  { input: [0, 1], output: [1] },
-  { input: [1, 0], output: [1] },
-  { input: [1, 1], output: [0] }
-];
-
-net.train(trainingData);
-
-console.log(net.run([0, 0]));   
+console.log(brain)
